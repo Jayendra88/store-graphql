aeed6df12faeec1151164b3d928aafddd365ee5a
Dan <daniel.ddsc@gmail.com>
2018-11-05 17:43:57 -0200

    Fixes calculatedAttachments to return empty string

--- node/resolvers/catalog/calculatedAttachmentsResolver.ts
+++ node/resolvers/catalog/calculatedAttachmentsResolver.ts
@@ -242,6 +242,10 @@ export default async (
     }),
   })
 
+  if (!reducedAttachmentSchema) {
+    return ''
+  }
+
   const calculatedSchema = { ...schema, ...reducedAttachmentSchema }
 
   return JSON.stringify(calculatedSchema)
